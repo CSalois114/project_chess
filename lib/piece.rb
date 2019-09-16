c17bdafbc0833a2342628db1c3bfa83fc3289c8c
Chris <csalois@gmail.com>
2019-09-16 17:55:58 -0400

    update unicode syntax

--- lib/piece.rb
+++ lib/piece.rb
@@ -3,7 +3,7 @@ class Piece
 
   def initialize(unicode)
     @unicode = unicode
-    @color = ("2654".."2659").include?(@unicode) ? :white : :black
+    @color = ["\u2654","\u2655","\u2656","\u2657","\u2658","\u2659"].include?(@unicode) ? :white : :black
     @move_offsets = get_move_offsets
   end
  
@@ -15,17 +15,17 @@ class Piece
     all_diagnals    = [*-7..-1, *1..7].map{|n| [n,n]} + [*-7..-1, *1..7].map{|n| [n,-n]}
 
     case @type
-    when "2654" || "265A" #king
+    when "\u2654" || "\u265A" #king
       @move_offsets = [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1]]
-    when "2655" || "265B" #queen
+    when "\u2655" || "\u265B" #queen
       @move_offsets = all_horizontals + all_verticals + all_diagnals
-    when "2656" || "265C" #rook
+    when "\u2656" || "\u265C" #rook
       @move_offsets = all_horizontals + all_verticals
-    when "2657" || "265D" #bishop
+    when "\u2657" || "\u265D" #bishop
       @move_offsets = all_diagnals
-    when "2658" || "265E" #knight
+    when "\u2658" || "\u265E" #knight
       @move_offsets = [[2,-1], [2,1], [1,2], [-1,2], [-2,1], [-2,-1], [-1,-2], [1,-2]]
-    when "2659" || "265F" #pawn
+    when "\u2659" || "\u265F" #pawn
       @move_offsets = [[0,1], [0,2], [-1,1], [1,1]]
     else
       @move_offsets = nil
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,8 +2,8 @@ require "./lib/piece.rb"
 
 describe Piece do
  it "sets its color and returns it with #color" do
-  white_king = Piece.new("2654")
-  black_king = Piece.new("265A")
+  white_king = Piece.new("\u2654")
+  black_king = Piece.new("\u265A")
   expect(white_king.color).to eql(:white)
   expect(black_king.color).to eql(:black)
  end
