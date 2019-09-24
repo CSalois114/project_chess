27f0fe30ca912a2ea4e77e5c4e79fe37449311d2
Chris <csalois@gmail.com>
2019-09-24 19:48:03 -0400

    add #legal_move? method and change name of #legal_king_move? to #moving_into_check? and refactor and make private #legal_non_king_move? and #moving_into_check?

--- lib/board.rb
+++ lib/board.rb
@@ -20,46 +20,15 @@ class Board
     @squares[origin_coords].piece = nil
     @turn_color = @turn_color == :white ? :black : :white
   end
-  
-  def legal_non_king_move?(origin_coords, destination_coords, test_run=false)
+
+  def legal_move?(origin_coords, destination_coords, ignore_color=false)
     return false unless @squares[origin_coords] && @squares[destination_coords] && @squares[origin_coords].piece
-    #test_run should be set to true when you want to test a move out of turn of the game
-    #this is used by #check? to see if a piece could attack the king
-    unless test_run
-      return false unless @squares[origin_coords].piece.color == @turn_color
-    end
-    move_offsets = get_coord_offsets(origin_coords, destination_coords)
-    piece = @squares[origin_coords].piece
-    return false unless piece.move_offsets.include?(move_offsets)
-    #If the destination square is occupied
-    if @squares[destination_coords].piece
-      return false if piece.color == @squares[destination_coords].piece.color
-      #If the piece is a pawn, it can't attack straight ahead
-      return false if ["\u2659", "\u265F"].include?(piece.unicode) && 
-        [[0, 1],[0, 2],[0, -1],[0, -2]].include?(move_offsets)
-    end
-    #Check for clear line of attack if piece is a queen, rook, or bishop
-    if ["\u2655", "\u265B", "\u2656", "\u265C", "\u2657", "\u265D"].include?(piece.unicode)
-      return false unless line_to_destination_clear?(origin_coords, destination_coords)
-    end
-    #Pawns can only move two spaces if origin_square is the square they start on
-    return false if piece.unicode == "\u2659" && move_offsets == [0, 2] && origin_coords[1] != 2
-    return false if piece.unicode == "\u265F" && move_offsets == [0, -2] && origin_coords[1] != 7
-
-    return true
-  end
-
-  def legal_king_move?(origin_coords, destination_coords)
-    #See if it's a legal move disregarding if the king will be in check.
+    return false unless @squares[origin_coords].piece.color == @turn_color || ignore_color
     return false unless legal_non_king_move?(origin_coords, destination_coords)
-    #Saves destination squares piece if there is one so that it can temporarily be replaced
-    #with the moving king to see if it will be in check at the destination.
-    destination_current_piece = @squares[destination_coords].piece
-    @squares[destination_coords].piece = @squares[origin_coords].piece
-    legal = check?(destination_coords) ? false : true
-    #Replace the piece that should currently be at the destination.
-    @squares[destination_coords].piece = destination_current_piece
-    legal
+    if @squares[origin_coords].piece && ["\u2654", "\u265A"].include?(@squares[origin_coords].piece.unicode)
+      return false unless moving_into_check?(origin_coords, destination_coords)
+    end
+    true
   end
 
   def check?(king_coords)
@@ -69,7 +38,7 @@ class Board
     enemy_squares_array.each do |square|
       #If any enemy has the potential to attack the king, check if that move is legal.
       if square.piece.move_offsets.include?(get_coord_offsets(square.coords.dup, king_coords))
-         return true if legal_non_king_move?(square.coords.dup, king_coords, true)
+         return true if legal_move?(square.coords.dup, king_coords, true)
       end
     end
     return false
@@ -79,12 +48,12 @@ class Board
     return false unless check?(king_coords) 
     @squares[king_coords].piece.move_offsets.each do |offsets|
       destination_coords = [king_coords[0] + offsets[0], king_coords[1] + offsets[1]]
-      return false if @squares[destination_coords] && legal_king_move?(king_coords, destination_coords)
+      return false if @squares[destination_coords] && legal_move?(king_coords, destination_coords)
     end
     true
   end
 
-  def get_winner
+  def get_result
     @squares.values.each do |square| 
       return :white if square.piece && square.piece.unicode == "\u265A" && checkmate?(square.coords)
       return :black if square.piece && square.piece.unicode == "\u2654" && checkmate?(square.coords)
@@ -107,6 +76,39 @@ class Board
   end
    
   private
+
+  def legal_non_king_move?(origin_coords, destination_coords)
+    move_offsets = get_coord_offsets(origin_coords, destination_coords)
+    piece = @squares[origin_coords].piece
+    return false unless piece.move_offsets.include?(move_offsets)
+    #If the destination square is occupied
+    if @squares[destination_coords].piece
+      return false if piece.color == @squares[destination_coords].piece.color
+      #If the piece is a pawn, it can't attack straight ahead
+      return false if ["\u2659", "\u265F"].include?(piece.unicode) && 
+        [[0, 1],[0, 2],[0, -1],[0, -2]].include?(move_offsets)
+    end
+    #Check for clear line of attack if piece is a queen, rook, or bishop
+    if ["\u2655", "\u265B", "\u2656", "\u265C", "\u2657", "\u265D"].include?(piece.unicode)
+      return false unless line_to_destination_clear?(origin_coords, destination_coords)
+    end
+    #Pawns can only move two spaces if origin_square is the square they start on
+    return false if piece.unicode == "\u2659" && move_offsets == [0, 2] && origin_coords[1] != 2
+    return false if piece.unicode == "\u265F" && move_offsets == [0, -2] && origin_coords[1] != 7
+
+    return true
+  end
+
+  def moving_into_check?(origin_coords, destination_coords)
+    #Saves destination squares piece if there is one so that it can temporarily be replaced
+    #with the moving king to see if it will be in check at the destination.
+    destination_current_piece = @squares[destination_coords].piece
+    @squares[destination_coords].piece = @squares[origin_coords].piece
+    legal = check?(destination_coords) ? false : true
+    #Replace the piece that should currently be at the destination.
+    @squares[destination_coords].piece = destination_current_piece
+    legal
+  end
 
   def line_to_destination_clear?(origin_coords, destination_coords)
     offsets = get_coord_offsets(origin_coords, destination_coords)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -29,77 +29,72 @@ describe Board do
     end
   end 
 
-  describe "#legal_non_king_move?" do
+  describe "#legal_move?" do
     it "returns false when no piece is selected" do
       board = Board.new
-      expect(board.legal_non_king_move?([1, 5], [1, 6], true)).to be false
+      expect(board.legal_move?([1, 5], [1, 6], true)).to be false
     end
 
     it "returns false when the origin or destination square doesn't exist" do
       board = Board.new
-      expect(board.legal_non_king_move?([8, 1], [9, 1], true)).to be false
-      expect(board.legal_non_king_move?([9, 3], [8, 3], true)).to be false
+      expect(board.legal_move?([8, 1], [9, 1], true)).to be false
+      expect(board.legal_move?([9, 3], [8, 3], true)).to be false
     end
 
     it "returns false if destination is not included in move offsets for that piece" do
       board = Board.new
-      expect(board.legal_non_king_move?([1, 2], [1, 5], true)).to be false
-      expect(board.legal_non_king_move?([2, 1], [4, 3], true)).to be false
+      expect(board.legal_move?([1, 2], [1, 5], true)).to be false
+      expect(board.legal_move?([2, 1], [4, 3], true)).to be false
     end
 
     it "handles pawn doulble move only from base line" do
       board = Board.new
       board.squares[[1,5]].piece = Piece.new("\u2659")
-      expect(board.legal_non_king_move?([1, 2], [1, 4], true)).to be true
-      expect(board.legal_non_king_move?([1, 5], [1, 7], true)).to be false
-      expect(board.legal_non_king_move?([2, 7], [2, 5], true)).to be true
+      expect(board.legal_move?([1, 2], [1, 4], true)).to be true
+      expect(board.legal_move?([1, 5], [1, 7], true)).to be false
+      expect(board.legal_move?([2, 7], [2, 5], true)).to be true
     end
 
     it "doesnt allow pieces to attack their own color" do
       board = Board.new
-      expect(board.legal_non_king_move?([5, 1], [5, 2], true)).to be false
-      expect(board.legal_non_king_move?([2, 1], [4, 2], true)).to be false
+      expect(board.legal_move?([5, 1], [5, 2], true)).to be false
+      expect(board.legal_move?([2, 1], [4, 2], true)).to be false
     end
 
     it "only allows pawns to attack on diagnal" do
       board = Board.new
       board.squares[[3,3]].piece = Piece.new("\u265F")
       board.squares[[4,4]].piece = Piece.new("\u265F")
-      expect(board.legal_non_king_move?([3, 2], [3, 3], true)).to be false
-      expect(board.legal_non_king_move?([4, 2], [4, 4], true)).to be false
-      expect(board.legal_non_king_move?([4, 2], [3, 3], true)).to be true
+      expect(board.legal_move?([3, 2], [3, 3], true)).to be false
+      expect(board.legal_move?([4, 2], [4, 4], true)).to be false
+      expect(board.legal_move?([4, 2], [3, 3], true)).to be true
     end
     
     it "doesn't let the queens, rooks, or bishops jump over pieces" do
       board = Board.new
       (1..8).each {|x| board.squares[[x,7]].piece = nil}
 
-      expect(board.legal_non_king_move?([1, 1], [1, 3], true)).to be false
-      expect(board.legal_non_king_move?([1, 8], [1, 3], true)).to be true
-      expect(board.legal_non_king_move?([1, 8], [1, 2], true)).to be true
+      expect(board.legal_move?([1, 1], [1, 3], true)).to be false
+      expect(board.legal_move?([1, 8], [1, 3], true)).to be true
+      expect(board.legal_move?([1, 8], [1, 2], true)).to be true
 
-      expect(board.legal_non_king_move?([3, 1], [1, 3], true)).to be false
-      expect(board.legal_non_king_move?([3, 8], [1, 6], true)).to be true
+      expect(board.legal_move?([3, 1], [1, 3], true)).to be false
+      expect(board.legal_move?([3, 8], [1, 6], true)).to be true
       
-      expect(board.legal_non_king_move?([4, 1], [4, 3], true)).to be false
-      expect(board.legal_non_king_move?([4, 8], [4, 3], true)).to be true
+      expect(board.legal_move?([4, 1], [4, 3], true)).to be false
+      expect(board.legal_move?([4, 8], [4, 3], true)).to be true
     end
-  end
 
-  describe "#legal_king_move?" do
     it "doesn't allow you to move into check" do
       board = Board.new
       board.squares[[1, 5]].piece = Piece.new("\u2654")
       board.squares[[5, 7]].piece = nil
       
-      expect(board.legal_king_move?([1, 5], [1, 4])).to be true
-      expect(board.legal_king_move?([1, 5], [1, 6])).to be false
-      expect(board.legal_king_move?([1, 5], [2, 5])).to be true
-      expect(board.legal_king_move?([1, 5], [2, 4])).to be false
-
-      board.turn_color = :black
-
-      expect(board.legal_king_move?([5, 8], [5, 7])).to be true
+      expect(board.legal_move?([1, 5], [1, 4], true)).to be true
+      expect(board.legal_move?([1, 5], [1, 6], true)).to be false
+      expect(board.legal_move?([1, 5], [2, 5], true)).to be true
+      expect(board.legal_move?([1, 5], [2, 4], true)).to be false
+      expect(board.legal_move?([5, 8], [5, 7], true)).to be true
     end
   end
   
@@ -132,10 +127,10 @@ describe Board do
     end
   end
 
-  describe "get_winner" do
+  describe "get_result" do
     it "returns nil when there is no winner" do
       board = Board.new
-      expect(board.get_winner).to be nil
+      expect(board.get_result).to be nil
     end
 
     it "returns the winning color when a king is in checkmate" do
@@ -144,14 +139,14 @@ describe Board do
       board.squares[[2, 4]].piece = Piece.new("\u265B")
       board.squares[[1, 6]].piece = Piece.new("\u2654")
 
-      expect(board.get_winner).to eql(:black)
+      expect(board.get_result).to eql(:black)
 
       board = Board.new
       board.squares[[5, 8]].piece = nil
       board.squares[[2, 5]].piece = Piece.new("\u2655")
       board.squares[[1, 3]].piece = Piece.new("\u265A")
 
-      expect(board.get_winner).to eql(:white)
+      expect(board.get_result).to eql(:white)
     end
     
     it "returns :draw if there are only the two kings left" do
@@ -160,7 +155,7 @@ describe Board do
       board.squares[[1, 1]].piece = Piece.new("\u2654")
       board.squares[[8, 8]].piece = Piece.new("\u265A")
 
-      expect(board.get_winner).to eql(:draw)
+      expect(board.get_result).to eql(:draw)
     end
   end
 end
